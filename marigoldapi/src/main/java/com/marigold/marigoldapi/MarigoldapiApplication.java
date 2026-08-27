fe415957ef6d2c04ff222169f19a7233bf269bfe
mattpall321 <mattpallik@gmail.com>
2026-08-27 16:50:42 -0400

    Springboot intializer setup

---
+++ marigoldapi/src/main/java/com/marigold/marigoldapi/MarigoldapiApplication.java
@@ -0,0 +1,13 @@
+package com.marigold.marigoldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarigoldapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarigoldapiApplication.class, args);
+	}
+
+}
---
+++ marigoldapi/src/test/java/com/marigold/marigoldapi/MarigoldapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marigold.marigoldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarigoldapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
